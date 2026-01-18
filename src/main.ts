c07531cfb0e95d8b7da9e1f97113d3aad5de3f1f
Moorts <moritz.gutfleisch@gmx.de>
2026-01-18 16:50:34 +0100

    Remove annoying sample thingy

--- src/main.ts
+++ src/main.ts
@@ -83,12 +83,6 @@ export default class MathArchivist extends Plugin {
 
 		// This adds a settings tab so the user can configure various aspects of the plugin
 		this.addSettingTab(new ArchivistSettingTab(this.app, this));
-
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			new Notice("Click");
-		});
 	}
 
 	onunload() {
